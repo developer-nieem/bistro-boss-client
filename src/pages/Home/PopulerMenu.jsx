905309de08807bbad0a7f67be743c467affea79f
developer-nieem <naimhossenwork@gmail.com>
2023-05-25 15:59:41 +0600

    Create Custom Hook Usemenu To Load Menu Data

--- src/pages/Home/PopulerMenu.jsx
+++ src/pages/Home/PopulerMenu.jsx
@@ -1,26 +1,20 @@
-import { useEffect, useState } from "react";
+
 import SectionTitle from "../../components/SectionTitle";
 import MenuItems from "../Shared/MenuItems/MenuItems";
+import useMenu from "../../Hooks/useMenu";
 
 const PopulerMenu = () => {
-    const [items , setItems] =  useState([])
 
-    useEffect(()=> {
-        fetch('/menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const popularItem =  data.filter(item => item.category === 'popular');
-            setItems(popularItem)
-        })
-    },[])
-
+    const [items] =  useMenu();
+    const populars =  items.filter( item => item.category === "popular")
+  
     return (
         <div className="max-w-screen-lg m-auto my-10">
             <SectionTitle heading='FROM OUR MENU' subHeading='Check it Popular Menu'></SectionTitle>
 
             <div className="md:grid md:grid-cols-2 gap-5">
                 {
-                    items.map(item => <MenuItems key={item._id} item={item}></MenuItems>)
+                    populars.map(item => <MenuItems key={item._id} item={item}></MenuItems>)
                 }
             </div>
         </div>
--- src/pages/OurMenu/OurMenu/OurMenu.jsx
+++ src/pages/OurMenu/OurMenu/OurMenu.jsx
@@ -2,7 +2,7 @@ import Cover from "../../Shared/Cover/Cover";
 
 import topCover from "../../../assets/menu/banner3.jpg";
 import SectionTitle from "../../../components/SectionTitle";
-import PopulerMenu from "../../Home/PopulerMenu";
+
 const OurMenu = () => {
   return (
     <div>
@@ -17,24 +17,7 @@ const OurMenu = () => {
           heading='TODAY"S OFFER '
           subHeading='Don"t miss'
         ></SectionTitle>
-        <PopulerMenu></PopulerMenu>
-        <Cover
-        img={topCover}
-        title="OUR MENU"
-        details="Would you like to try a dish?"
-      ></Cover>
-        <PopulerMenu></PopulerMenu>
-        <Cover
-        img={topCover}
-        title="OUR MENU"
-        details="Would you like to try a dish?"
-      ></Cover>
-        <PopulerMenu></PopulerMenu>
-        <Cover
-        img={topCover}
-        title="OUR MENU"
-        details="Would you like to try a dish?"
-      ></Cover>
+        
       </div>
     </div>
   );
